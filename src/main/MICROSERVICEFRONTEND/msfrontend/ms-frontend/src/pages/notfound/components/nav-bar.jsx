8a226eaaa852dfdce5583837f81efd3ac995179c
HabaySecureId <92311631+HabaySecureId@users.noreply.github.com>
2022-04-11 08:10:32 +0100

    react icons installation

---
+++ src/main/MICROSERVICEFRONTEND/msfrontend/ms-frontend/src/pages/notfound/components/nav-bar.jsx
@@ -0,0 +1,9 @@
+import { BsFillAlarmFill } from "react-icons/bs";
+const NavBar = ()=>{
+    return(
+<nav className="navbar navbar-expand navbar-dark bg-dark">
+<BsFillAlarmFill/>
+</nav>
+    )
+}
+export{NavBar};
