12cb2a043dd9447f282e094e0a8c372bd5898348
HabaySecureId <92311631+HabaySecureId@users.noreply.github.com>
2022-04-11 13:26:50 +0100

    axios config

---
+++ src/main/MICROSERVICEFRONTEND/msfrontend/ms-frontend/src/model/purchase.js
@@ -0,0 +1,11 @@
+export default class Purchase{
+    constructor(userId,courseId,title,price,purchaseTime,id){
+       this.userId=userId;
+       this.courseId=courseId;
+       this.title=title;
+       this.price=price;
+       this.purchaseTime=purchaseTime;
+       this.id=id;
+
+    }
+}
---
+++ src/main/MICROSERVICEFRONTEND/msfrontend/ms-frontend/src/services/authentication.service.js
@@ -0,0 +1,11 @@
+import { BASE_API_URL } from "../common.js/Common";
+
+const BASE_URL= BASE_API_URL + 'api/authentication';
+class AuthenticationService{
+
+    login(user){
+        
+    }
+
+}
+export default new AuthenticationService();
