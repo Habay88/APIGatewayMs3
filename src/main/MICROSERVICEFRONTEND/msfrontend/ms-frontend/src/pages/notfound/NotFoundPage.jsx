3ac3928965e4da1c9c3b6ffdea95f857be4b3e11
HabaySecureId <92311631+HabaySecureId@users.noreply.github.com>
2022-04-11 11:52:55 +0100

    updated error pages

--- src/main/MICROSERVICEFRONTEND/msfrontend/ms-frontend/src/pages/notfound/NotFoundPage.jsx
+++ src/main/MICROSERVICEFRONTEND/msfrontend/ms-frontend/src/pages/notfound/NotFoundPage.jsx
@@ -1,7 +1,21 @@
-
+import {Link} from 'react-router-dom';
 const NotFoundPage = ()=>{
     return (
-        <p>Not Found Page</p>
+    <div className="container">
+        <div className="row">
+         <div className="col-md-12 text-center">
+           <span className="display-1">
+               404
+           </span>  
+           <div className="mb-4 lead">
+Oops!.We can't seem to find the page you are looking for.
+           </div>
+    <Link to="/home" className="btn btn-link">
+        Back to Home
+    </Link>
+             </div>   
+        </div>
+    </div>
     )
 }
 
--- src/main/MICROSERVICEFRONTEND/msfrontend/ms-frontend/src/pages/unauthorized/UnauthorizedPage.jsx
+++ src/main/MICROSERVICEFRONTEND/msfrontend/ms-frontend/src/pages/unauthorized/UnauthorizedPage.jsx
@@ -1,7 +1,21 @@
-
+import {Link} from 'react-router-dom';
 const UnauthorizedPage = ()=>{
     return (
-        <p>Unauthorized Page</p>
+        <div className="container">
+        <div className="row">
+         <div className="col-md-12 text-center">
+           <span className="display-1">
+               401
+           </span>  
+           <div className="mb-4 lead">
+Unauthorized!.Access to this is denied.
+           </div>
+    <Link to="/home" className="btn btn-link">
+        Back to Home
+    </Link>
+             </div>   
+        </div>
+    </div>
     )
 }
 
